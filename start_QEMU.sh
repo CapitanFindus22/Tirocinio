25713b05ae4e882336b5dd246fe86e0f274301b9
CapitanFindus <106431916+CapitanFindus22@users.noreply.github.com>
2025-06-03 16:57:45 +0200

    Delete start_QEMU.sh

--- start_QEMU.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-qemu-system-x86_64 -s -S -m 256M -kernel linux-6.14.7/arch/x86_64/boot/bzImage -initrd rootfs.cpio.gz -append "root=/dev/mem" &
-sleep 1
-seergdb ./linux-6.14.7/vmlinux --silent 
-#Inserire target remote :1234 in gdb commands before "run"
