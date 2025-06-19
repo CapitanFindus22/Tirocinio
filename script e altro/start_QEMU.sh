8e5edf7957dc5d9bc10821a990153a66a9be7daf
leonardo <ganzaroli.leonardo@gmail.com>
2025-06-19 15:48:31 +0200

    Switch to PCIe

--- script e altro/start_QEMU.sh
+++ script e altro/start_QEMU.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-qemu-system-x86_64 -s -S -m 256M -kernel linux-6.14.7/arch/x86_64/boot/bzImage -initrd rootfs.cpio.gz -append "root=/dev/mem" &
+qemu-system-x86_64 -s -S -m 256M -kernel linux-6.14.7/arch/x86_64/boot/bzImage -initrd rootfs.cpio.gz -append "root=/dev/mem" -machine q35 -device disp,bus=pcie.0 -monitor stdio &
 sleep 1
-seergdb ./linux-6.14.7/vmlinux --silent 
+seergdb ./linux-6.14.7/vmlinux --silent target remote:1234
 #Inserire target remote :1234 in gdb commands before "run"
